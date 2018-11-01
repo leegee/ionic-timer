f6fa88ed43d9be57e93864dede1cc6fbf57ea7d5
Lee Goddard <leegee@gmail.com>
2018-11-01 13:30:49 +0100

    tidy; text

--- src/app/about/about.page.ts
+++ src/app/about/about.page.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-about',
   templateUrl: 'about.page.html',
   styleUrls: ['about.page.scss']
 })
-export class AboutPage {}
+export class AboutPage {
+  slideOptions = {
+    effect: 'flip',
+    delay: 6000
+  };
+  product = environment.product;
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,10 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 export const environment = {
   production: false,
+  product: {
+    name: 'Your Timers',
+    version: 0.1
+  },
   logging: {
     logLevels: [
       {
