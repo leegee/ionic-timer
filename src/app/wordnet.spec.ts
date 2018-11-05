a5f2c406c454f4427a9149c4cd1935df43950df7
Lee Goddard <leegee@gmail.com>
2018-11-05 18:52:26 +0100

    Extending

--- src/app/wordnet.spec.ts
+++ src/app/wordnet.spec.ts
@@ -12,37 +12,57 @@ const verbDataFilepath = path.resolve('./src/assets/wordnet/data.verb');
 
 describe('binary-file-search', () => {
     it('inits data file paths', () => {
-        Wordnet.DATA_PATHS.forEach(filepath => {
-            expect(fs.existsSync(filepath), filepath + ' exists').to.equal(true);
-        })
-
+        Object.keys(Wordnet.dataFiles).forEach(filetypeKey => {
+            expect(
+                fs.existsSync(Wordnet.dataFiles[filetypeKey].path),
+                fs.existsSync(Wordnet.dataFiles[filetypeKey].path) + ' exists'
+            ).to.equal(true);
+        });
     });
 
-    it('finds the line containing "import"', async () => {
+    it('findLineContainingWord finds the line containing "import"', async () => {
         expect(
-            await Wordnet.findLineContaining(verbDataFilepath, 'import')
+            await Wordnet.findLineContainingWord('import', 'v')
         ).to.equal(
             // tslint:disable-next-line:max-line-length
-            '02232722 40 v 01 import 1 003 @ 02232190 v 0000 ;c 06128570 n 0000 ! 02232877 v 0101 01 + 08 00 | transfer (electronic data) into a database or document  '
+            '02232722 40 v 01 import 1 003 @ 02232190 v 0000 ;c 06128570 n 0000 ! 02232877 v 0101 01 + 08 00 | transfer (electronic data) into a database or document'
         );
     });
 
-    it('finds the line containing "import" from all files', async () => {
+    it('findAllLinesContainingWord finds the line containing "import" from all files', async () => {
         expect(
-            await Wordnet.findDefinition('import')
+            await Wordnet.findAllLinesContainingWord('import')
         ).to.deep.equal(
             // tslint:disable:max-line-length
             [
-                '02346136 40 v 01 import 0 011 @ 02260362 v 0000 ;c 01090446 n 0000 + 03564667 n 0102 + 01111750 n 0102 + 10200531 n 0102 + 10200531 n 0101 + 03564667 n 0101 + 10201366 n 0101 + 01111750 n 0101 ! 02346409 v 0101 ~ 02345856 v 0000 02 + 08 00 + 21 00 | bring in from abroad  ',
-                '03564667 06 n 02 import 0 importation 0 004 @ 03076708 n 0000 + 02346136 v 0201 + 02346136 v 0101 ! 03306207 n 0101 | commodities (goods or services) bought from a foreign country  '
+                '02346136 40 v 01 import 0 011 @ 02260362 v 0000 ;c 01090446 n 0000 + 03564667 n 0102 + 01111750 n 0102 + 10200531 n 0102 + 10200531 n 0101 + 03564667 n 0101 + 10201366 n 0101 + 01111750 n 0101 ! 02346409 v 0101 ~ 02345856 v 0000 02 + 08 00 + 21 00 | bring in from abroad',
+                '03564667 06 n 02 import 0 importation 0 004 @ 03076708 n 0000 + 02346136 v 0201 + 02346136 v 0101 ! 03306207 n 0101 | commodities (goods or services) bought from a foreign country'
             ]
             // tslint:enable:max-line-length
         );
     });
 
+    it('finds line by sysnet offset', async () => {
+        const [, num] = '02346409'.match(/^(\d+)$/);
+        expect(
+            await Wordnet.findLineContainingSysnetOffset('v', Number(num))
+        ).to.equal(
+            // tslint:disable-next-line:max-line-length
+            '02346409 40 v 01 export 0 009 @ 02260362 v 0000 ;c 01090446 n 0000 + 03306207 n 0102 + 01111952 n 0102 + 03306207 n 0101 + 10073634 n 0101 + 01111952 n 0101 ! 02346136 v 0101 ~ 02345856 v 0000 03 + 08 00 + 16 00 + 21 00 | sell or transfer abroad; "we export less than we import and have a negative trade balance"'
+        );
+    });
+
     xit('finds "export" is the opposite of "import"', async () => {
         expect(
-            await Wordnet.findOppositeOf('import')
-        ).to.equal('export');
+            // tslint:disable-next-line:max-line-length
+            Wordnet._findOppositeFromLine('02346136 40 v 01 import 0 011 @ 02260362 v 0000 ;c 01090446 n 0000 + 03564667 n 0102 + 01111750 n 0102 + 10200531 n 0102 + 10200531 n 0101 + 03564667 n 0101 + 10201366 n 0101 + 01111750 n 0101 ! 02346409 v 0101 ~ 02345856 v 0000 02 + 08 00 + 21 00 | bring in from abroad')
+        ).to.deep.equal({
+            'pointerSymbol': '!',
+            'pos': 'v',
+            'source': '01',
+            'pointerSysnetOffset': '02346409',
+            'target': '01',
+        });
     });
+
 });
--- src/app/wordnet.ts
+++ src/app/wordnet.ts
@@ -3,88 +3,101 @@ import * as fs from 'fs';
 import * as util from 'util';
 
 const read = util.promisify(fs.read);
-const INPUT_BUFFER_SIZE = 256;
+const INPUT_BUFFER_READ_LINE_SIZE = 256;
 
-const Paths2FH = {};
+export interface RegExpMatchArrayX10d extends RegExpMatchArray {
+    groups: {};
+}
 
-export class Wordnet {
+export interface RegExpMatchArrayGetSysnetPointerReGroups extends RegExpMatchArray {
+    groups: GetSysnetPointerReGroups;
+}
 
-    static DATA_PATHS = Wordnet._initPaths('src/assets/wordnet');
+export interface GetBasicSysnetInfoRe {
+    wordSynsetOffset: number;
+    wordLexFilenum: string;
+    wordSsType: string;
+    word: string;
+}
 
-    static _initPaths(relativeRootDir: string): string[] {
-        const rv = [];
-        ['adj', 'adv', 'noun', 'verb'].forEach(suffix => {
-            rv.push(
-                path.resolve(relativeRootDir + '/data.' + suffix)
-            );
-        });
-        return rv;
+export interface GetSysnetPointerReGroups {
+    pointerSymbol: string;
+    pointerSysnetOffset: number;
+    pos: string;
+    source: string;
+    target: string;
+}
+
+export class DataFile {
+
+    static descriptorCache: { [key: string]: number } = {};
+
+    suffix: string;
+    type: string;
+    path: string;
+
+    constructor(suffix: string, type: string, filepath: string) {
+        this.suffix = suffix;
+        this.type = type;
+        this.path = filepath;
     }
 
-    /**
-     * Closes all open file descriptors.
-     */
-    static destroy(): void {
-        Object.keys(Paths2FH).forEach(key => {
-            Paths2FH[key].close();
-        });
-    }
-
-    /**
-     * Get a filehandle for a filepath.
-     * @param filepath File path of a Wordnet data file.
-     * @returns A file descriptor.
-     * @throws If the file is not found or cannot be opened.
-     */
-    static getFilehandle(filepath: string): number {
+    get descriptor() {
         let FH;
-        if (Paths2FH[filepath]) {
-            FH = Paths2FH[filepath];
+        if (DataFile.descriptorCache[this.type]) {
+            FH = DataFile.descriptorCache[this.type];
         } else {
             try {
-                FH = fs.openSync(filepath, 'r');
-                Paths2FH[filepath] = FH;
+                FH = fs.openSync(this.path, 'r');
+                DataFile.descriptorCache[this.type] = FH;
             } catch (e) {
                 throw e;
             }
         }
         return FH;
     }
+}
 
-    static async findOppositeOf(subject: string) { }
+export class Wordnet {
+    // tslint:disable-next-line:max-line-length
+    static GET_BASIC_SYSNET_INFO_RE = /^(?<wordSynsetOffset>\d+)\s(?<wordLexFilenum>\d\d)\s(?<wordSsType>[nvasr])\s(?<wCnt>\d\d)(?<word>\S+)\s/;
+    static GET_SYSNET_POINTER_RE = /(?<pointerSymbol>\!)\s(?<pointerSysnetOffset>\d+)\s(?<pos>\w)\s(?<source>\d\d)(?<target>\d\d)/;
+
+    static dataFiles: { [key: string]: DataFile } = {
+        r: new DataFile('adj', 'r', path.resolve('src/assets/wordnet/data.adj')),
+        a: new DataFile('adv', 'a', path.resolve('src/assets/wordnet/data.adv')),
+        n: new DataFile('noun', 'n', path.resolve('src/assets/wordnet/data.noun')),
+        v: new DataFile('verb', 'v', path.resolve('src/assets/wordnet/data.verb'))
+    };
 
     /**
-     * Find the Wordnet lien that defines a word.
-     * @param subject The word
+     * Parse (only the antonym) from a data file line
+     * @param line Line to parse
+     * @see https://wordnet.princeton.edu/documentation/wndb5wn
+     * @see https://wordnet.princeton.edu/documentation/wninput5wn
      */
-    static async findDefinition(subject: string): Promise<string[]> {
-        const promises = [];
-        const rvs = [];
-        Wordnet.DATA_PATHS.forEach(filepath => {
-            promises.push(
-                Wordnet.findLineContaining(filepath, subject).then(rv => {
-                    if (rv !== null) {
-                        rvs.push(rv);
-                    }
-                })
-            );
-        });
-        await Promise.all(promises);
-        return rvs;
+    static async _findOppositeFromLine(line: string): Promise<string> {
+        // ! 02346409 v 0101 ~ 02345856 v 0000 02 + 08 00 + 21 00 | bring in from abroad')
+        const result = Wordnet.GET_SYSNET_POINTER_RE.exec(line) as RegExpMatchArrayGetSysnetPointerReGroups;
+        const foundLine = await Wordnet.findLineContainingSysnetOffset(
+            result.groups.pos,
+            Number(result.groups.pointerSysnetOffset)
+        );
+        return foundLine;
     }
 
-    /**
-     * Performs a binary search to get the line containing the specified Wordnet subject from specified filepath.
-     * @param filepath Path to a Wordnet data file.
-     * @param subject The subject to find.
-     * @return A `Promise` resolving in the found line, or `null`.
-     */
-    static async findLineContaining(filepath: string, subject: string): Promise<string | null> {
-        const FH = Wordnet.getFilehandle(filepath);
-        const inputBuffer = Buffer.alloc(INPUT_BUFFER_SIZE);
-        const stats = fs.fstatSync(FH);
-        const pos = Math.floor(stats.size / 2);
-        return Wordnet._findLineContaining(subject.toLocaleLowerCase(), FH, inputBuffer, pos, stats.size);
+    static async findLineContainingSysnetOffset(filetype: string, sysnetOffset: number): Promise<string | null> {
+        const FH = Wordnet.dataFiles[filetype].descriptor;
+        const inputBuffer = Buffer.alloc(INPUT_BUFFER_READ_LINE_SIZE);
+        const readRv = await read(FH, inputBuffer, 0, inputBuffer.byteLength, sysnetOffset);
+        let line = readRv.buffer.toString();
+
+        if (line.indexOf('\n') === -1) {
+            const finalReadRv = await read(FH, inputBuffer, 0, inputBuffer.byteLength, sysnetOffset + INPUT_BUFFER_READ_LINE_SIZE);
+            line = line + finalReadRv.buffer.toString().substring(0, finalReadRv.buffer.indexOf('\n'));
+        }
+
+        return line.trimRight();
     }
 
     static async _findLineContaining(
@@ -115,7 +128,7 @@ export class Wordnet {
                 line = line + finalReadRv.buffer.toString().substring(0, finalReadRv.buffer.indexOf('\n'));
             }
             // console.info('\n\nFINAL FOUND LINE [%s] ', line);
-            return line;
+            return line.trimRight();
         }
 
         if (comparision < 0) {
@@ -131,5 +144,49 @@ export class Wordnet {
         }
 
         return Wordnet._findLineContaining(subject, FH, inputBuffer, pos, totalLength);
+    }
+
+    /**
+     * Performs a binary search to get the line containing the specified Wordnet subject from specified filepath.
+     * @param subject The subject to find.
+     * @param filetypeKey Key describing the file type - @see TODO
+     * @return A `Promise` resolving in the found line, or `null`.
+     */
+    static async findLineContainingWord(subject: string, filetypeKey: string): Promise<string | null> {
+        const FH = Wordnet.dataFiles[filetypeKey].descriptor;
+        const inputBuffer = Buffer.alloc(INPUT_BUFFER_READ_LINE_SIZE);
+        const stats = fs.fstatSync(FH);
+        const pos = Math.floor(stats.size / 2);
+        return Wordnet._findLineContaining(subject.toLocaleLowerCase(), FH, inputBuffer, pos, stats.size);
+    }
+
+    /**
+     * Find the Wordnet lien that defines a word.
+     * @param subject The word
+     */
+    static async findAllLinesContainingWord(subject: string): Promise<string[]> {
+        const promises = [];
+        const rvs = [];
+        Object.keys(Wordnet.dataFiles).forEach(filetypeKey => {
+            promises.push(
+                Wordnet.findLineContainingWord(subject, filetypeKey).then(rv => {
+                    if (rv !== null) {
+                        // rvs.push(Wordnet.parseDatafileLine(rv));
+                        rvs.push(rv);
+                    }
+                })
+            );
+        });
+        await Promise.all(promises);
+        return rvs;
+    }
+
+    /**
+     * Closes all open file descriptors.
+     */
+    static destroy(): void {
+        Object.keys(Wordnet.dataFiles).forEach(filetypeKey => {
+            Wordnet.dataFiles[filetypeKey].descriptor.close();
+        });
     }
 }
